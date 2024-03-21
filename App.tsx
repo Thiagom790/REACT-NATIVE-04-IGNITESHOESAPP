e80584b11fa88929a075b7f7b1611376cf72a616
thiago teixeira <thiagom790@gmail.com>
2024-03-21 09:58:00 -0300

    feat: add example of message buttons

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { OneSignal } from "react-native-onesignal";
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal";
 
 import { Routes } from "./src/routes";
 
@@ -13,6 +13,7 @@ import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
+import { useEffect } from "react";
 // import {
 //   tagUserEmailCreate,
 //   tagUserEmailRemove,
@@ -29,6 +30,33 @@ export default function App() {
   // tagUserEmailCreate("test@test.com");
   // tagUserEmailRemove();
   // tagUserInfoCreate();
+
+  // Example da utilização de botões adicionais no OneSignal
+  // useEffect(() => {
+  //   const handleNotificationClicked = (event: NotificationClickEvent) => {
+  //     const actionId = event.result.actionId; // id do botão clicado
+
+  //     if (actionId === "1") {
+  //       console.info("Ver todos");
+  //     }
+
+  //     if (actionId === "2") {
+  //       console.info("Ver pedido");
+  //     }
+  //   };
+
+  //   OneSignal.Notifications.addEventListener(
+  //     "click",
+  //     handleNotificationClicked
+  //   );
+
+  //   return () => {
+  //     OneSignal.Notifications.removeEventListener(
+  //       "click",
+  //       handleNotificationClicked
+  //     );
+  //   };
+  // }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
